96926a8f9165eea9e4bec12703214850b169828d
michikosuwoto <michikosuwoto@gmail.com>
2021-04-06 22:51:50 -0700

    Adding timestamp

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   document.querySelector(".city").innerHTML = response.data.name;
@@ -8,6 +25,12 @@ function displayTemperature(response) {
   );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = response.data.wind.speed;
+
+  //still need to get the icon for the weather description here
+  //also need to activate fuction of getting forecast here
+
+  let dateElement = document.querySelector(".day-date-time");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "3535582e190a7084b5cf1cb8af8b7750";
