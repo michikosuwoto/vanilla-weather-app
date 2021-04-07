cefb3b66a762713bb297e33ed9e5f2f6ca6fc603
michikosuwoto <michikosuwoto@gmail.com>
2021-04-06 22:17:58 -0700

    Add function: show current weather information

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-alert("hello world");
+function displayTemperature(response) {
+  console.log(response.data);
+  document.querySelector(".city").innerHTML = response.data.name;
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "3535582e190a7084b5cf1cb8af8b7750";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
