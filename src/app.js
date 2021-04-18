a6e6ce9349293bcfc72d00c5173b2fc3fb2f7379
michikosuwoto <michikosuwoto@gmail.com>
2021-04-18 11:08:00 -0700

    Moving HTML forecast to JS

--- src/app.js
+++ src/app.js
@@ -15,6 +15,37 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+                <div class="forecast-day">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="38"
+                />
+                <div class="forecast-temperature">
+                  <span class="forecast-temperature-max"> 18° </span>
+                  <span class="forecast-temperature-min"> 12° </span>
+                </div>
+  </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayWeather(response) {
   console.log(response.data);
   document.querySelector(".city").innerHTML = response.data.name;
@@ -82,3 +113,4 @@ let form = document.querySelector("#weatherapp-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Los Angeles");
+displayForecast();
